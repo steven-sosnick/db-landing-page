8d99ca387baa6528a086c1f5526ce61da790e6df
Steven Sosnick <scsosnick@gmail.com>
2022-11-02 11:35:17 -0400

    adding colors and buttons

--- src/components/MainHero.tsx
+++ src/components/MainHero.tsx
@@ -7,9 +7,11 @@ const MainHero = () => {
   return (
     <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
       <div className="sm:text-center lg:text-left">
-        <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+        <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl xs:text-4xl">
           <span className="block xl:inline">{mainHero.title}</span>{' '}
-          <span className={`block text-primary xl:inline`}>
+          <span
+            className={`text-4xl block text-primary sm:inline whitespace-nowrap`}
+          >
             {mainHero.subtitle}
           </span>
         </h1>
@@ -20,15 +22,23 @@ const MainHero = () => {
           <div className="rounded-md shadow">
             <a
               href={mainHero.primaryAction.href}
-              className={`w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-background bg-primary hover:bg-border hover:text-primary md:py-4 md:text-lg md:px-10`}
+              className={`w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-background bg-primary hover:bg-border hover:text-background md:py-4 md:text-lg md:px-10`}
             >
               {mainHero.primaryAction.text}
             </a>
           </div>
           <div className="mt-3 sm:mt-0 sm:ml-3">
             <a
+              href={mainHero.thirdAction.href}
+              className={`w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md border-primary text-secondary bg-background hover:bg-border hover:text-background md:py-4 md:text-lg md:px-10`}
+            >
+              {mainHero.thirdAction.text}
+            </a>
+          </div>
+          <div className="mt-3 sm:mt-0 sm:ml-3">
+            <a
               href={mainHero.secondaryAction.href}
-              className={`w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md border-primary text-secondary bg-background hover:bg-border hover:text-primary md:py-4 md:text-lg md:px-10`}
+              className={`w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md border-primary text-secondary bg-background hover:bg-border hover:text-background md:py-4 md:text-lg md:px-10`}
             >
               {mainHero.secondaryAction.text}
             </a>
